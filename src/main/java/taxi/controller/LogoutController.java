5916825c84d35a344b64b3de4a41cc510a0f4551
Koziarevskyi <koziarevskyi@gmail.com>
2021-07-05 18:10:26 +0300

    C009 - Added logger

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -5,12 +5,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import taxi.lib.Injector;
+import taxi.service.DriverService;
 
 public class LogoutController extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger(LogoutController.class);
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        long driverId = Long.parseLong(req.getParameter("driver_id"));
+        String login = driverService.get(driverId).getLogin();
         req.getSession().invalidate();
+        logger.info("user with login \"{}\" logout", login);
         resp.sendRedirect("/login");
     }
 }
