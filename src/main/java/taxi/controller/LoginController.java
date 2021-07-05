44a5dfd0ff5f1129bda2746cbb2609891be37dcc
Koziarevskyi <koziarevskyi@gmail.com>
2021-07-05 16:54:32 +0300

    C008 - Added logger

--- src/main/java/taxi/controller/LoginController.java
+++ src/main/java/taxi/controller/LoginController.java
@@ -6,12 +6,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import taxi.lib.Injector;
 import taxi.lib.exception.AuthenticationException;
 import taxi.model.Driver;
 import taxi.service.AuthenticationService;
 
 public class LoginController extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger(LoginController.class);
     private static final String DRIVER_ID = "driver_id";
     private static final Injector injector = Injector.getInstance("taxi");
     private static final AuthenticationService authenticationService =
@@ -32,8 +35,10 @@ public class LoginController extends HttpServlet {
             Driver driver = authenticationService.login(login, password);
             HttpSession session = req.getSession();
             session.setAttribute(DRIVER_ID, driver.getId());
+            logger.info("User with login \"{}\" - login", login);
             resp.sendRedirect("/index");
         } catch (AuthenticationException cause) {
+            logger.error("Can't login user", cause);
             req.setAttribute("errorMsg", cause.getMessage());
             req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
         }
