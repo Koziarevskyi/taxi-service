cf6bfc8ecabe524979f065fa0cb7f0ac42c51b01
Koziarevskyi <koziarevskyi@gmail.com>
2021-07-05 18:30:34 +0300

    C010 - Some fix

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -5,12 +5,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import taxi.lib.Injector;
 import taxi.service.DriverService;
 
 public class LogoutController extends HttpServlet {
+    private static final String DRIVER_ID = "driver_id";
     private static final Logger logger = LogManager.getLogger(LogoutController.class);
     private static final Injector injector = Injector.getInstance("taxi");
     private final DriverService driverService = (DriverService) injector
@@ -19,7 +21,8 @@ public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        long driverId = Long.parseLong(req.getParameter("driver_id"));
+        HttpSession session = req.getSession();
+        Long driverId = (Long) session.getAttribute(DRIVER_ID);
         String login = driverService.get(driverId).getLogin();
         req.getSession().invalidate();
         logger.info("user with login \"{}\" logout", login);
